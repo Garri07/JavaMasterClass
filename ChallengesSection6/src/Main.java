75a29c101503c491dc0874701fc3fe5f826bdb4e
Alexandra <24434807+Garri07@users.noreply.github.com>
2020-06-11 15:30:32 +0200

    Update Main.java
    Added comment from GitHub after setting up tracking

--- ChallengesSection6/src/Main.java
+++ ChallengesSection6/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         //Some comment for the Git MasterClass on Udemy
         //Comment added from GitHub
+        //Another comment added on GitHub after setting up tracking
     }
 }
