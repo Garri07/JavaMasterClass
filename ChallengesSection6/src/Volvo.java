fd8c03d8641b4056785a5c56ad4a7e03e0d54366
Alexandra Garrido Jaque <alexandra_garrido@outlook.com>
2020-06-12 11:37:43 +0200

    Update Volvo.java

--- ChallengesSection6/src/Volvo.java
+++ ChallengesSection6/src/Volvo.java
@@ -3,11 +3,11 @@ public class Volvo extends Car {
     private String model;
     private String color;
 
-    public Volvo(int seats, double weight, int size, int wheels, String engineType, String brand, int doors, int gears, String model, String color) {
-        super(seats, weight, size, wheels, engineType, brand, doors, gears);
+    public Volvo(int seats, double weight, int size, int wheels, String engineType, int doors, int gears, String model, String color) {
+        super(seats, weight, size, wheels, engineType, "Volvo", doors, gears);
         this.model = model;
         this.color = color;
     }
 
-    
+
 }
